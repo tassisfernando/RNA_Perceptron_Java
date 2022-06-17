a97616ffa2bbf35187700b7bd4e00608ecd06b15
Tássis Fernando <tassis.fernandes@dtidigital.com.br>
2022-06-17 14:14:21 -0300

    Finalizando versão inicial - pre testes

--- src/model/Perceptron.java
+++ src/model/Perceptron.java
@@ -4,24 +4,23 @@ import math.MathUtils;
 
 import java.util.Random;
 
+import static java.lang.System.arraycopy;
+
 public class Perceptron {
     private Double ni;
     private int qtdIn;
     private int qtdOut;
     private Double[][] w;
-    private Double[][] deltaW;
 
-    private final static Double RANGE_MIN = -0.3;
-    private final static Double RANGE_MAX = 0.3;
+    private final static Double RANGE_MIN = -0.2;
+    private final static Double RANGE_MAX = 0.2;
 
     public Perceptron(int qtdIn, int qtdOut, Double ni) {
         this.qtdIn = qtdIn;
         this.qtdOut = qtdOut;
         this.ni = ni;
         this.w = new Double[qtdIn + 1][qtdOut];
-        this.deltaW = new Double[qtdIn + 1][qtdOut];
 
-        // TODO gerar os pesos aleatórios
         this.gerarRandomW();
     }
 
@@ -29,23 +28,22 @@ public class Perceptron {
         Random random = new Random();
         for(int i = 0; i < w.length; i++) {
             for(int j = 0; j < w[0].length; j++) {
-                double randomValue = random.nextDouble() * (RANGE_MAX - RANGE_MIN) + RANGE_MIN;
+                w[i][j] = random.nextDouble() * (RANGE_MAX - RANGE_MIN) + RANGE_MIN;
             }
         }
-
     }
 
     public Double[] learn(Double[] xIn, Double[] y) {
         Double[] x = new Double[xIn.length + 1];
-
-        // copiar xIn em x e colocar x[x.length - 1] = 1
         Double[] out = new Double[y.length];
+
+        generateXArray(xIn, x);
+
         for (int j = 0; j < y.length; j++) {
             double u = 0;
             for (int i = 0; i < x.length; i++) {
                 u += x[i] * w[i][j];
             }
-            // TODO testar a função sig (1/1+e^(-u))
             out[j] = MathUtils.sig(u);
         }
 
@@ -55,5 +53,10 @@ public class Perceptron {
             }
         }
         return out;
+    }
+
+    public void generateXArray(Double[] xIn, Double[] x) {
+        arraycopy(xIn, 0, x, 0, xIn.length);
+        x[x.length - 1] = 1D;
     }
 }
--- src/runner/PerceptronRunner.java
+++ src/runner/PerceptronRunner.java
@@ -2,33 +2,77 @@ package runner;
 
 import model.Perceptron;
 
+import static java.lang.Math.abs;
+
 public class PerceptronRunner {
 
-    private static Double[][][] baseE = new Double[][][]{
-            { {0D, 0D}, {0D} },
-            { {0D, 1D}, {0D} },
-            { {1D, 0D}, {0D} },
-            { {1D, 1D}, {1D} },
+    /* AND */
+    private static Double[][][] database = {
+            { { 0D, 0D }, { 0D } },
+            { { 0D, 1D }, { 0D } },
+            { { 1D, 0D }, { 0D } },
+            { { 1D, 1D }, { 1D } }
     };
 
+
+    /* XOR
+    private static Double[][][] database = {
+            { { 0D, 0D }, { 0D } },
+            { { 0D, 1D }, { 1D } },
+            { { 1D, 0D }, { 1D } },
+            { { 1D, 1D }, { 0D } }
+    };
+    */
+
+    /* OR
+    private static Double[][][] database = {
+            { { 0D, 0D }, { 0D } },
+            { { 0D, 1D }, { 1D } },
+            { { 1D, 0D }, { 1D } },
+            { { 1D, 1D }, { 1D } }
+    };
+    */
+
+    /* Robô
+    private static Double[][][] database = {
+            { { 0D, 0D, 0D }, { 1D, 1D } },
+            { { 0D, 0D, 1D }, { 0D, 1D } },
+            { { 0D, 1D, 0D }, { 1D, 0D } },
+            { { 0D, 1D, 1D }, { 0D, 1D } },
+            { { 1D, 0D, 0D }, { 1D, 0D } },
+            { { 1D, 0D, 1D }, { 1D, 0D } },
+            { { 1D, 1D, 0D }, { 1D, 0D } },
+            { { 1D, 1D, 1D }, { 1D, 0D } }
+    }; */
+
     public static void main(String[] args) {
-        Perceptron perceptron = new Perceptron(2, 1, 0.1);
+        final Double NI = 0.1;
+        final int N_EPOCAS = 1000;
+
+        Perceptron perceptron = new Perceptron(database[0][0].length, database[0][1].length, NI);
         Double erroEp = 0D, erroAm = 0D;
 
-        for(int e = 0; e < 1000; e++) {
+        for(int e = 0; e < N_EPOCAS; e++) {
             erroEp = 0D;
-            for(int a = 0; a < baseE.length; a++) {
-                Double[] x = baseE[a][0];
-                Double[] y = baseE[a][1];
+            for (Double[][] sample : database) {
+                Double[] x = sample[0];
+                Double[] y = sample[1];
                 Double[] out = perceptron.learn(x, y);
 
-                //TODO calcular o erro da amostra
-                //erroAm = somatorio(mod | yj - Tetaj |) j=0 -> K
-
+                erroAm = sumErro(y, out);
                 erroEp += erroAm;
             }
 
-            System.out.printf("Época: %s - Erro: %s", e, erroEp);
+            System.out.printf("Época: %s - Erro: %6f\n", (e+1), erroEp);
         }
+    }
+
+    private static Double sumErro(Double[] y, Double[] out) {
+        double sum = 0D;
+        for(int i = 0; i < y.length; i++) {
+            sum += abs(y[i] - out[i]);
+        }
+
+        return sum;
     }
 }
